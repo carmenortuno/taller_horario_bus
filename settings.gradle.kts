6da08968f48334c5458ed7d3a31e596adc6d67e4
carmenortuno <carmenortuno@elcampico.org>
2024-01-16 10:00:54 +0100

    base proyecto

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "taller_horario_bus"
+include(":app")
+ 
