d50acbf9c8aab864c62beba4f38a253ad128ec6c
carmenortuno <carmenortuno@elcampico.org>
2024-01-17 14:56:06 +0100

    adaptador

---
+++ app/src/main/java/com/example/taller_horario_bus/ParadasBusAdapter.kt
@@ -0,0 +1,14 @@
+package com.example.taller_horario_bus
+
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.taller_horario_bus.database.horario.Horario
+import java.text.SimpleDateFormat
+
+
+class ParadasBusAdapter{
+}
